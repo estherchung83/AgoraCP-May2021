2f9996b6403b6fe2bf24cab7a8485025552b35dc
Esther Chung <eschung@Esthers-MacBook-Pro.local>
2021-05-14 20:37:51 -0700

    updated

---
+++ hw2.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Homework 2 - Javascript Calculator</title>
+</head>
+
+<body>
+	<input type="number" id="input1">
+	<input type="number" id="input2">
+
+	<input type="button" id="add">
+	<input type="button" id="subtract">
+	<input type="button" id="multiply">
+	<input type="button" id="divide">
+</body>
+
+</html>
